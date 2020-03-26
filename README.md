6ef94ad9653416d23458e6adcee615a752615859
Ubuntu <ubuntu@ip-172-31-3-186.us-east-2.compute.internal>
2020-03-26 20:38:10 +0000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+eightb/ebitter/8bitter
+
+game engine that has very little, except aesthetically, to do with 8-bits. react + redux. to clone:
+
+> git clone https://github.com/mullen-programs/eightb.git
+> npx create-react-app ebitter
+> mv eightb/src/* ebitter/src/
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div id="landscape"><p>landscape</p></div>
+      <div id="detail"><p>detail</p></div>
+      <div id="narrative"><p>narrative</p></div>
+      <div id="choices"><p>choices</p></div>
+    </div>
+  );
+}
+
+export default App;
